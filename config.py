4de11970bfbea2d98307d54e20d4a55508d96c21
vivekascoder <vivekascoder@gmail.com>
2021-07-10 13:59:15 +0530

    Implemented simple Websocker Async Server and it's client in VueJs

---
+++ config.py
@@ -0,0 +1,5 @@
+
+import os
+
+BASE_DIR = os.getcwd()
+CMD_PATH = os.path.join(BASE_DIR, 'commands.json')
--- rimoto.py
+++ rimoto.py
@@ -1,7 +1,36 @@
 #!/usr/bin/env python
 
 import asyncio
-import websocket
+import websockets
 import pyautogui
 import os
+import json
+
+from config import CMD_PATH
+
+def get_commands(file_path: str) -> dict:
+  if not os.path.isfile(file_path):
+    return False
+  file = open(file_path, 'r').read()
+  data = json.loads(file)
+  return data
+
+
+async def server(websocket, path):
+  commands = get_commands(CMD_PATH)
+  while True:
+    message = await websocket.recv()
+    if message in commands:
+      pyautogui.hotkey(*commands[message])
+    else:
+      print('Command not found %' %(message))
+
+def main():
+  print("Starting Server 0.0.0.0:5678")
+  start_server = websockets.serve(server, '0.0.0.0', 5678)
+  asyncio.get_event_loop().run_until_complete(start_server)
+  asyncio.get_event_loop().run_forever()
+
+if __name__ == '__main__':
+  main()
 
